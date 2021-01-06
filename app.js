cb466825ea3576dafab0787fb984aa65a80c9ff9
Sean Kim <sean.m.kim@gmail.com>
2021-01-06 12:31:53 -0800

    Completed lab, and added /all and save feature

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 /** Simple demo Express app. */
 
 const express = require("express");
+const fsP = require("fs/promises");
 const app = express();
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
 
 const { convertStrNums } = require("./utils");
 const {
@@ -18,25 +19,97 @@ const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
 app.get("/mean", function (req, res, next) {
+  if (!req.query.nums) throw new BadRequestError(MISSING);
+  
   let numStrArr = req.query.nums.split(",");
   let numsArr = convertStrNums(numStrArr);
 
   let mean = findMean(numsArr);
   
-  return res.json({
+  let content = {
     response: {
       operation: "mean",
       value: mean,
+    }
+  };
+
+  if (req.query.save === "true") {
+    writeToFile(JSON.stringify(content));
+  }
+  
+  return res.json(content);
+
+});
+
+/* Takes string content and writes it to results.json file */
+async function writeToFile(content) {
+  try {
+    await fsP.writeFile("./results.json", content, "utf8");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  
+  console.log("Successfully wrote file");
+}
+
+/** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res, next) {
+  if (!req.query.nums) throw new BadRequestError(MISSING);
+
+  let numStrArr = req.query.nums.split(",");
+  let numsArr = convertStrNums(numStrArr);
+
+  let median = findMedian(numsArr);
+  
+  return res.json({
+    response: {
+      operation: "median",
+      value: median,
     }
   });
 
 });
 
-/** Finds median of nums in qs: returns {operation: "median", result } */
-
-
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get("/mode", function (req, res, next) {
+  if (!req.query.nums) throw new BadRequestError(MISSING);
 
+  let numStrArr = req.query.nums.split(",");
+  let numsArr = convertStrNums(numStrArr);
+
+  let mode = findMode(numsArr);
+  
+  return res.json({
+    response: {
+      operation: "mode",
+      value: mode,
+    }
+  });
+
+});
+
+/** Finds mean, median, and mode of nums in qs: returns {operation: "mean", "median", "mode", result } */
+app.get("/all", function (req, res, next) {
+  if (!req.query.nums) throw new BadRequestError(MISSING);
+
+  let numStrArr = req.query.nums.split(",");
+  let numsArr = convertStrNums(numStrArr);
+
+  let mean = findMean(numsArr);
+  let median = findMedian(numsArr);
+  let mode = findMode(numsArr);
+  
+  return res.json({
+    response: {
+      operation: "all",
+      mean: mean,
+      median: median,
+      mode: mode,
+    }
+  });
+
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
